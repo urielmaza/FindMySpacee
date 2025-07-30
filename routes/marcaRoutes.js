a690402138057249ce460e2a507c72d58a152d56
urielmaza <urielmaza38@gmail.com>
2025-07-30 02:04:40 -0300

    aca solo queda el front

--- routes/marcaRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/marcaRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getMarcas } = require('../controllers/marcaController');
-
-// Ruta para listar marcas
-router.get('/marcas', getMarcas);
-
-module.exports = router;
