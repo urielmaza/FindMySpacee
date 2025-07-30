72d68d5eb7ed15c5b8b2e1db3866dc978514584e
urielmaza <urielmaza38@gmail.com>
2025-07-30 02:37:29 -0300

    se agrego configuracion para el servidor render

--- fiindmyspace/src/main.jsx
+++ fiindmyspace/src/main.jsx
@@ -1,11 +1,13 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
-// src/main.jsx
--- fiindmyspace/vite.config.js
+++ fiindmyspace/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/FindMySpacee/',  // <-- IMPORTANTE: el nombre del repo con slash al final
   plugins: [react()],
   server: {
     proxy: {
