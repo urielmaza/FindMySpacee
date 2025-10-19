c45957991977f859be799b7684c3455af0108407
torricofiona <torricofionaa@gmail.com>
2025-10-19 15:54:44 -0300

    Subir estacionamiento corregido

--- fiindmyspace/src/pages/SubirEstacionamiento.jsx
+++ fiindmyspace/src/pages/SubirEstacionamiento.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import BannerUser from '../components/BannerUser';
 import { getUserSession } from '../utils/auth';
-import styles from './cargarVehiculo.module.css'; // Reutilizamos las mismas clases
+import styles from './cargarVehiculo.module.css';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
